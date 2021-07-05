93416cea0ac047ba84d39556e102bcbb0ac2e128
Ryan Salvanera <ryan.salvanera@gmail.com>
2021-07-05 21:47:18 +0800

    Basic Ingredient

---
+++ ingredient.py
@@ -0,0 +1,6 @@
+class Ingredient:
+    def __init__(self, quantity, unit, ingredient) -> None:
+        self.quantity = quantity
+        self.unit_of_measurement = unit
+        self.ingredient = ingredient
+        pass
